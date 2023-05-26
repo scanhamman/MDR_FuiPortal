c34c05c67649696c8d79485a57c7d6d28184ab7b
Steve Canham <stevecanham@outlook.com>
2023-05-26 01:18:50 +0100

    More work on search page

---
+++ Shared/Country.cs
@@ -0,0 +1,13 @@
+﻿namespace MDR_FuiPortal.Shared;
+
+public class Country
+{
+    public int id { get; set; }
+    public string name { get; set; }
+
+    public Country(int _id, string _name)
+    {
+        id = _id;
+        name = _name;
+    }
+}
